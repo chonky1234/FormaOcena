e68a4988043d54bf06db400b90a4218b6f1b03e6
chonky1234 <118490337+chonky1234@users.noreply.github.com>
2025-05-06 22:59:55 +0200

    FormaDesign

--- FormaOcena/FormaOcena/Form1.Designer.cs
+++ FormaOcena/FormaOcena/Form1.Designer.cs
@@ -30,12 +30,268 @@ namespace FormaOcena
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.txtIme = new System.Windows.Forms.TextBox();
+            this.txtPrezime = new System.Windows.Forms.TextBox();
+            this.txtAdresa = new System.Windows.Forms.TextBox();
+            this.txtJMBG = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtUloga = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnSkrozLevo = new System.Windows.Forms.Button();
+            this.btnLevo = new System.Windows.Forms.Button();
+            this.btnDodaj = new System.Windows.Forms.Button();
+            this.btnIzmeni = new System.Windows.Forms.Button();
+            this.btnBrisi = new System.Windows.Forms.Button();
+            this.btnDesno = new System.Windows.Forms.Button();
+            this.btnSkrorzDesno = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(640, 88);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(100, 22);
+            this.txtID.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // txtIme
+            // 
+            this.txtIme.Location = new System.Drawing.Point(640, 136);
+            this.txtIme.Name = "txtIme";
+            this.txtIme.Size = new System.Drawing.Size(100, 22);
+            this.txtIme.TabIndex = 2;
+            // 
+            // txtPrezime
+            // 
+            this.txtPrezime.Location = new System.Drawing.Point(640, 185);
+            this.txtPrezime.Name = "txtPrezime";
+            this.txtPrezime.Size = new System.Drawing.Size(100, 22);
+            this.txtPrezime.TabIndex = 3;
+            // 
+            // txtAdresa
+            // 
+            this.txtAdresa.Location = new System.Drawing.Point(640, 237);
+            this.txtAdresa.Name = "txtAdresa";
+            this.txtAdresa.Size = new System.Drawing.Size(100, 22);
+            this.txtAdresa.TabIndex = 4;
+            // 
+            // txtJMBG
+            // 
+            this.txtJMBG.Location = new System.Drawing.Point(640, 281);
+            this.txtJMBG.Name = "txtJMBG";
+            this.txtJMBG.Size = new System.Drawing.Size(100, 22);
+            this.txtJMBG.TabIndex = 5;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(640, 326);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 22);
+            this.txtEmail.TabIndex = 6;
+            // 
+            // txtUloga
+            // 
+            this.txtUloga.Location = new System.Drawing.Point(640, 365);
+            this.txtUloga.Name = "txtUloga";
+            this.txtUloga.Size = new System.Drawing.Size(100, 22);
+            this.txtUloga.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(445, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(445, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Ime";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(445, 185);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 17);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Prezime";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(445, 242);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 17);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Adresa";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(445, 284);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 17);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "JMBG";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(445, 326);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 17);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "E-mail";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(445, 365);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(45, 17);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Uloga";
+            // 
+            // btnSkrozLevo
+            // 
+            this.btnSkrozLevo.Location = new System.Drawing.Point(157, 508);
+            this.btnSkrozLevo.Name = "btnSkrozLevo";
+            this.btnSkrozLevo.Size = new System.Drawing.Size(75, 23);
+            this.btnSkrozLevo.TabIndex = 15;
+            this.btnSkrozLevo.Text = "<<";
+            this.btnSkrozLevo.UseVisualStyleBackColor = true;
+            // 
+            // btnLevo
+            // 
+            this.btnLevo.Location = new System.Drawing.Point(285, 508);
+            this.btnLevo.Name = "btnLevo";
+            this.btnLevo.Size = new System.Drawing.Size(75, 23);
+            this.btnLevo.TabIndex = 16;
+            this.btnLevo.Text = "<";
+            this.btnLevo.UseVisualStyleBackColor = true;
+            // 
+            // btnDodaj
+            // 
+            this.btnDodaj.Location = new System.Drawing.Point(511, 508);
+            this.btnDodaj.Name = "btnDodaj";
+            this.btnDodaj.Size = new System.Drawing.Size(75, 23);
+            this.btnDodaj.TabIndex = 17;
+            this.btnDodaj.Text = "Dodaj";
+            this.btnDodaj.UseVisualStyleBackColor = true;
+            // 
+            // btnIzmeni
+            // 
+            this.btnIzmeni.Location = new System.Drawing.Point(665, 508);
+            this.btnIzmeni.Name = "btnIzmeni";
+            this.btnIzmeni.Size = new System.Drawing.Size(75, 23);
+            this.btnIzmeni.TabIndex = 18;
+            this.btnIzmeni.Text = "Izmeni";
+            this.btnIzmeni.UseVisualStyleBackColor = true;
+            // 
+            // btnBrisi
+            // 
+            this.btnBrisi.Location = new System.Drawing.Point(842, 508);
+            this.btnBrisi.Name = "btnBrisi";
+            this.btnBrisi.Size = new System.Drawing.Size(75, 23);
+            this.btnBrisi.TabIndex = 19;
+            this.btnBrisi.Text = "Brisi";
+            this.btnBrisi.UseVisualStyleBackColor = true;
+            // 
+            // btnDesno
+            // 
+            this.btnDesno.Location = new System.Drawing.Point(967, 508);
+            this.btnDesno.Name = "btnDesno";
+            this.btnDesno.Size = new System.Drawing.Size(75, 23);
+            this.btnDesno.TabIndex = 20;
+            this.btnDesno.Text = ">";
+            this.btnDesno.UseVisualStyleBackColor = true;
+            // 
+            // btnSkrorzDesno
+            // 
+            this.btnSkrorzDesno.Location = new System.Drawing.Point(1102, 508);
+            this.btnSkrorzDesno.Name = "btnSkrorzDesno";
+            this.btnSkrorzDesno.Size = new System.Drawing.Size(75, 23);
+            this.btnSkrorzDesno.TabIndex = 21;
+            this.btnSkrorzDesno.Text = ">>";
+            this.btnSkrorzDesno.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1267, 679);
+            this.Controls.Add(this.btnSkrorzDesno);
+            this.Controls.Add(this.btnDesno);
+            this.Controls.Add(this.btnBrisi);
+            this.Controls.Add(this.btnIzmeni);
+            this.Controls.Add(this.btnDodaj);
+            this.Controls.Add(this.btnLevo);
+            this.Controls.Add(this.btnSkrozLevo);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUloga);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtJMBG);
+            this.Controls.Add(this.txtAdresa);
+            this.Controls.Add(this.txtPrezime);
+            this.Controls.Add(this.txtIme);
+            this.Controls.Add(this.txtID);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox txtIme;
+        private System.Windows.Forms.TextBox txtPrezime;
+        private System.Windows.Forms.TextBox txtAdresa;
+        private System.Windows.Forms.TextBox txtJMBG;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtUloga;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnSkrozLevo;
+        private System.Windows.Forms.Button btnLevo;
+        private System.Windows.Forms.Button btnDodaj;
+        private System.Windows.Forms.Button btnIzmeni;
+        private System.Windows.Forms.Button btnBrisi;
+        private System.Windows.Forms.Button btnDesno;
+        private System.Windows.Forms.Button btnSkrorzDesno;
+        private System.Windows.Forms.TextBox txtID;
     }
 }
 
